aef43d3c61a73c56b6da758c1a5bda381ff9cc21
Ketaki Tech <154119865+KetakiKahure@users.noreply.github.com>
2026-05-05 23:41:33 +0530

    Update CORS origin to include production URL

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app=express();
 // ))
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "https://getaiinterview-client.onrender.com"],
   credentials: true
 }));
 
